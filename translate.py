87a20fc6752aaf79ed74f87a1d0e201813ac05c5
manucho254 <manuchoadero@gmail.com>
2021-06-10 17:22:59 +0300

    'translator'

--- translate.py
+++ translate.py
@@ -1,19 +1,19 @@
-from tkinter import *
+from tkinter import Tk, Button,END, Label, Text, StringVar, Frame
 from googletrans import Translator
 from tkinter import ttk
-from tkinter.messagebox import *
+from tkinter.messagebox import showerror
 import threading
 
 class translated(Frame):
     def __init__(self, master):
         self.master = master
         self.master.title('Translate')
-        self.label = Label(self.master,  text='Translator', font='Helvetica, 20 bold',  bg='black', fg='white')
-        self.label.pack(side='top', fill='both')
+        self.labelTranslate = Label(self.master,  text='Translator', font='Helvetica, 20 bold',  bg='black', fg='white')
+        self.labelTranslate.pack(side='top', fill='both')
 
-        self.label = Label(self.master,  
+        self.labelChooseLanguage = Label(self.master,  
                        text='Choose language', font='Helvetica, 15 bold', width=40,  bg='white', fg='black')
-        self.label.place(x=260, y=41)
+        self.labelChooseLanguage.place(x=260, y=41)
         
         self.textEntered = Text(self.master, width=40, font='italic, 14 bold', height=15)
         self.textEntered.place(x=40, y=110)
@@ -21,24 +21,26 @@ class translated(Frame):
         self.textTranslated.place(x=520, y=110)
 
         default = StringVar()
-        val = default.get()
+        val = default.get()# get the value before being deleted by garbage collection
 
-        defaults = ttk.Combobox(
+        self.defaults = ttk.Combobox(
             self.master, width=20, textvariable=val, state='readonly', font=('verdana', 10, 'bold'))
 
-        defaults['values'] = ('English')
+        self.defaults['values'] = ('English')
 
-        defaults.place(x=190, y=80)
-        defaults.current(0)
+        self.defaults.place(x=190, y=80)
+        self.defaults.current(0)
+
+        #=============================================================#
 
         value = StringVar()
-        keepValue = value.get()
+        keepValue = value.get() # get the value before being deleted by garbage collection
 
-        chooseLangauge = ttk.Combobox(
+        self.chooseLangauge = ttk.Combobox(
             self.master, width=20, textvariable=keepValue, state='readonly', font=('verdana', 10, 'bold'))
 
-
-        chooseLangauge['values'] = (
+        #========instert all the language values to our combobox============
+        self.chooseLangauge['values'] = (
             'Afrikaans','Albanian','Arabic',
             'Armenian',' Azerbaijani','Basque','Belarusian','Bengali',
             'Bosnian','Bulgarian',' Catalan','Cebuano','Chichewa','Chinese','Corsican',
@@ -54,37 +56,40 @@ class translated(Frame):
             'Tamil','Tatar','Telugu','Thai','Turkish','Turkmen','Ukrainian','Urdu','Uyghur','Uzbek',
             'Vietnamese','Welsh','Xhosa''Yiddish','Yoruba','Zulu',)
 
-        chooseLangauge.place(x=610, y=80)
-        chooseLangauge.current(19)
+        self.chooseLangauge.place(x=610, y=80)
+        self.chooseLangauge.current(19)
 
         translated = Translator()
-
+       
+       # =========clear all the textboxes =========#
         def clear():
             self.textEntered.delete(1.0, END)
             self.textTranslated.delete(1.0, END)
 
+       #=====main function to translate the languages====
         def getAndTranslate():
             try:
-                lang = chooseLangauge.get()
+                lang = self.chooseLangauge.get() # get the language value from combobox
                 enteredText = self.textEntered.get(1.0, END)
-                translations = translated.translate(f'{enteredText}', dest=lang)
+                translations = translated.translate(enteredText, dest=lang)
                 self.textTranslated.delete(1.0, END)
                 self.textTranslated.insert(END, translations.text.title())
             except:
                 showerror('Error', 'try again')
 
+        #=====threading the function to prevent the gui from freezing========#
         def threaded():
             runs = threading.Thread(target=getAndTranslate)
             runs.start()
 
 
-        self.button = Button(self.master, text='Translate', bd=3, 
+        self.buttonTranslate = Button(self.master, text='Translate', bd=3, 
                    bg='black', fg='white', font=('Helvetica', 15 ), width=10, command=threaded)
-        self.button.place(x=350, y=450)
+        self.buttonTranslate.place(x=350, y=450)
 
-        self.button = Button(self.master, text='clear', bd=3,
+        self.buttonClear = Button(self.master, text='clear', bd=3,
                      bg='black', fg='white', width=10, font=('Helvetica', 15 ), command=clear)
-        self.button.place(x=530, y=450)
+        self.buttonClear.place(x=530, y=450)
 
 def main():
     root = Tk()
